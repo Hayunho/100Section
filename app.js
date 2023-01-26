9cadf4f536e93e5ba98070d772aa4d4f09a6e4c7
ha <hyunho0828@naver.com>
2023-01-26 21:51:17 +0900

    Remove HTML Element

--- app.js
+++ app.js
@@ -36,4 +36,5 @@ let firstH1Element = document.querySelector("h1");
 
 // 2. Remove it!
 
-firstH1Element.remove();
+// firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element); // for older browser
