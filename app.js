5a042e376b6146ca42ef1c1e745d320db79efddd
ha <hyunho0828@naver.com>
2023-01-26 19:19:35 +0900

    DOM Element first

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-console.dir(document);
+//console.dir(document);
 
-document.body.children[1].children[0].href = "https://google.com";
+//document.body.children[1].children[0].href = "https://google.com";
 
 // alert();
 // window.alert();
+//console.log();
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
