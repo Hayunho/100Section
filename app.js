381dcd063bf15dd36d44b72c1ea6280907058cf6
ha <hyunho0828@naver.com>
2023-01-26 21:58:04 +0900

    Move HTML Element

--- app.js
+++ app.js
@@ -16,25 +16,23 @@ anchorElement.href = "https://naver.com";
 
 // ADD AN ELEMENT
 // 1. Create the new element
-
 let newAnchorElement = document.createElement("a"); // Create anchor element
 newAnchorElement.href = "https://google.com";
 newAnchorElement.textContent = "This leads to google!";
 
 // 2. Get access to the parent element that should hold the new element
-
 let firstParagraph = document.querySelector("p");
 
 // 3. Insert the new element into the parent element content
-
 firstParagraph.append(newAnchorElement);
 
 // REMOVE ELEMENT
 // 1. Select the element that should be removed
-
 let firstH1Element = document.querySelector("h1");
 
 // 2. Remove it!
-
 // firstH1Element.remove();
 firstH1Element.parentElement.removeChild(firstH1Element); // for older browser
+
+// MOVE ELEMENT
+firstParagraph.parentElement.append(firstParagraph);
