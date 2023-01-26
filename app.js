55b0948c0fc15f2dde56433eeab5bd8855ad7590
ha <hyunho0828@naver.com>
2023-01-26 21:40:20 +0900

    Remove HTML Element

--- app.js
+++ app.js
@@ -28,3 +28,12 @@ let firstParagraph = document.querySelector("p");
 // 3. Insert the new element into the parent element content
 
 firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENT
+// 1. Select the element that should be removed
+
+let firstH1Element = document.querySelector("h1");
+
+// 2. Remove it!
+
+firstH1Element.remove();
