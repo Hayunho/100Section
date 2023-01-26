cbd83a8136d91110976b6d42ad0e9e0dd9212bd4
ha <hyunho0828@naver.com>
2023-01-26 19:32:14 +0900

    HTMl Elemet Query Method

--- app.js
+++ app.js
@@ -8,3 +8,8 @@
 
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector("p a"); // CSS -> p a { color: red;}
+anchorElement.href = "https://naver.com";
+// Match All
+// document.querySelectorAll();
