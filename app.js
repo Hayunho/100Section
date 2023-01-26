033d6b74425be5102912e1d75d3436e5e19d1aff
ha <hyunho0828@naver.com>
2023-01-26 20:42:26 +0900

    exercise practice

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
 
-anchorElement = document.querySelector("p a"); // CSS -> p a { color: red;}
+anchorElement = document.querySelector("p a"); // CSS Selector -> p a { color: red;}
 anchorElement.href = "https://naver.com";
 // Match All
 // document.querySelectorAll();
