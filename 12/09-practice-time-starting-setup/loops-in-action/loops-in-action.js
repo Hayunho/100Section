c2b79b58737eec4b1e968c139b60d44e25d822d5
ha <hyunho0828@naver.com>
2023-01-27 23:39:12 +0900

    Solved loops Example2

--- 12/09-practice-time-starting-setup/loops-in-action/loops-in-action.js
+++ 12/09-practice-time-starting-setup/loops-in-action/loops-in-action.js
@@ -18,3 +18,45 @@ function calculateSum() {
 }
 
 calculateSumButtonElement.addEventListener("click", calculateSum);
+
+// Second Example: Highlight Links
+
+const highlightLinksbuttonElement = document.querySelector(
+  "#highlight-links button"
+);
+
+function highlightLinks() {
+  const anchorElements = document.querySelectorAll("#highlight-links a");
+
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add("highlight");
+  }
+}
+
+highlightLinksbuttonElement.addEventListener("click", highlightLinks);
+
+//Display user data
+
+const dummyUserData = {
+  firstName: "Max",
+  lastName: "Schwarzmuller",
+  age: 32,
+};
+
+const displayUserDataButtonElement =
+  document.querySelector("#user-data button");
+
+function displayUserData() {
+  const outputDataElement = document.getElementById("output-user-data");
+
+  outputDataElement.innerHTML = "";
+
+  for (const key in dummyUserData) {
+    const newUserDataListItemElement = document.createElement("li");
+    const outputText = key.toUpperCase() + ": " + dummyUserData[key];
+    newUserDataListItemElement.textContent = outputText;
+    outputDataElement.append(newUserDataListItemElement);
+  }
+}
+
+displayUserDataButtonElement.addEventListener("click", displayUserData);
