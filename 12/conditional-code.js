fa77d7eb5c84c1fc27747515960ca8e5585b5078
ha <hyunho0828@naver.com>
2023-01-27 16:43:58 +0900

    practice if cluse

--- 12/conditional-code.js
+++ 12/conditional-code.js
@@ -3,3 +3,15 @@ const myName = "Ha";
 if (myName === "Ha") {
   console.log("Hello!");
 }
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("User is NOT logged in!");
+}
+
+const enteredUserName = "";
+
+if (enteredUserName) {
+  console.log("Input is valid");
+}
