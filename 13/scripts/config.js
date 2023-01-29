922e56ac9909853e4de54709eac5865bd7979ccb
ha <hyunho0828@naver.com>
2023-01-29 19:45:13 +0900

    Add config cancel

--- 13/scripts/config.js
+++ 13/scripts/config.js
@@ -6,4 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault(); // Https default(transmit) prevent --> browser reload prevent
+  const formData = new FormData(event.target); // blueprint formdata
+  const enteredPlayerName = formData.get("username").trim(); // "       Max    " --> "Max"
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
