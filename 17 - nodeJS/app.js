e34f98667fc4147c8d02c8160fffe49630de1b3d
ha <hyunho0828@naver.com>
2023-02-01 22:55:51 +0900

    practice expressJS

--- 17 - nodeJS/app.js
+++ 17 - nodeJS/app.js
@@ -1,20 +1,34 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequest(request, response) {
-  // localhost:3000/currenttime
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    //localhost:3000
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequest);
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000/currenttime;
 
-server.listen(3000);
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+}); // localhost:3000/
+
+app.listen(3000);
+
+// const http = require("http");
+
+// function handleRequest(request, response) {
+//   // localhost:3000/currenttime
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200; // 200->success, 404->not found ...
+//     response.end("<h1>" + new Date().toISOString() + "</h1>");
+//   } else if (request.url === "/") {
+//     //localhost:3000
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
 
 // amazon.com -> Send a request to Amazon's server
 // amazon.com:80
