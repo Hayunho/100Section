d63ee77ea8800c65c05be75754b9b6684d0efdb7
ha <hyunho0828@naver.com>
2023-02-01 21:40:55 +0900

    surface nodeJS

--- 17 - nodeJS/app.js
+++ 17 - nodeJS/app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end("<h1>Hello World!</h1>");
+  // localhost:3000/currenttime
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    //localhost:3000
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
 }
 
 const server = http.createServer(handleRequest);
