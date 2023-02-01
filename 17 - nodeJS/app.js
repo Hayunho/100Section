83118085e30db5eb806c27a1a4fde4c668519b36
ha <hyunho0828@naver.com>
2023-02-01 21:31:41 +0900

    practice nodeJS

--- 17 - nodeJS/app.js
+++ 17 - nodeJS/app.js
@@ -1,3 +1,16 @@
-const userName = "Maximilian";
+const http = require("http");
 
-console.log(userName);
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end("<h1>Hello World!</h1>");
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
+
+// amazon.com -> Send a request to Amazon's server
+// amazon.com:80
+// 80: Http port number
+// 443: more security port number
+// 3000: develop port number
