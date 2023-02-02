b3c2d12865907f4f8f4462824c53f82a8a43b822
ha <hyunho0828@naver.com>
2023-02-03 03:16:17 +0900

    input data

--- 19-more expressJS/app.js
+++ 19-more expressJS/app.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
+
+app.use(express.static("public")); // "public"폴더 안에 정적 CSS, JS 파일 등을 사용한다
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -17,6 +21,20 @@ app.get("/restaurants", function (req, res) {
 app.get("/recommend", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", function (req, res) {
