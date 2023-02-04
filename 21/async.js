55b5632c0b20fc6c9741c3b3e31718bbf956e861
ha <yunho081544@gmail.com>
2023-02-05 02:48:55 +0900

    Introduce promises

--- 21/async.js
+++ 21/async.js
@@ -1,18 +1,25 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   let fileData;
 
-  // fileData = fs.readFileSync("data.txt");
-  fileData = fs.readFile("data.txt", function (error, fileData) {
-    console.log("File parsing done!");
-    console.log(fileData.toString());
-  });
+  //1. fileData = fs.readFileSync("data.txt");
+
+  //2. fileData = fs.readFile("data.txt", function (error, fileData) {
+  //   console.log("File parsing done!");
+  //   console.log(fileData.toString());
+  // });
   // readFileSync : 파일을 다 읽기 전까지 다음 코드 실행 안함
   // readFile : (Async) 파일을 다 읽기 전이어도 다음 코드 실햄함
 
-  // console.log(fileData);
-  // console.log(fileData.toString());
+  //1. console.log(fileData);
+  //1. console.log(fileData.toString());
+
+  fs.readFile("data.txt").then(function (fileData) {
+    console.log("File parsing done!");
+    console.log(fileData.toString());
+  }); // promises
+
   console.log("Hi there!");
 }
 
