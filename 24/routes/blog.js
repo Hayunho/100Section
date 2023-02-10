48c7b4b818a37f65c654980a6e258784ad671520
ha <yunho081544@gmail.com>
2023-02-11 03:58:00 +0900

    add database

--- 24/routes/blog.js
+++ 24/routes/blog.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const db = require("../data/database");
+
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -10,8 +12,29 @@ router.get("/posts", function (req, res) {
   res.render("posts-list");
 });
 
-router.get("/new-post", function (req, res) {
-  res.render("create-post");
+router.get("/new-post", async function (req, res) {
+  const [authors] = await db.query("SELECT * FROM authors"); // db mysql authors TABLE에서 첫번째 배열 가져오기
+  res.render("create-post", {
+    authors: authors,
+  });
+});
+
+router.post("/posts", async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+
+  await db.query(
+    "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
+    [data]
+  );
+  // ? 안에 값 삽입, ? 개수 만큼 파라미터 지정 가능, ?가 하나면 변수 1개, ?가 2개면 변수 2개, 배열은 ? 하나만 있으면 됨
+  //=> db.query("INSERT INTO posts (title, summary, body, author_id) VALUES (?, ?, ?, ?)", [data[0], data[1], data[2], data[3]])
+
+  res.redirect("/posts");
 });
 
 module.exports = router;
